e38607e5c36df64e6a18f4f5fdbb3f81407ee873
destebanjv <davidjv312@gmail.com>
2025-07-16 11:22:51 -0500

    update

--- web/modules/custom/job_queue/src/Controller/JobQueueController.php
+++ web/modules/custom/job_queue/src/Controller/JobQueueController.php
@@ -7,14 +7,18 @@ use Drupal\Core\Controller\ControllerBase;
 class JobQueueController extends ControllerBase {
 
   public function enqueueJob() {
+    $uid = \Drupal::currentUser()->id();
+    $username = \Drupal::currentUser()->id();
     $queue = \Drupal::queue('job_queue_example');
     $queue->createItem([
+      'uid' => $uid,
+      'username' => $username,
       'time' => time(),
-      'message' => '¡Trabajo desde job_queue!',
+      'message' => '¡Trabajo generado por el usuario actual',
     ]);
 
     return [
-      '#markup' => $this->t('Trabajo agregado a la cola desde job_queue.'),
+      '#markup' => $this->t('Trabajo encolado por el usuario @user.', ['@user' => $username]),
     ];
   }
 }
