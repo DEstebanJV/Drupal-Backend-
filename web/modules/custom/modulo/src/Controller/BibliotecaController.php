0eff96eb5f875c135f6f064be923aa2c20abb83b
destebanjv <davidjv312@gmail.com>
2025-07-15 19:24:29 -0500

    new template in controller

--- web/modules/custom/modulo/src/Controller/BibliotecaController.php
+++ web/modules/custom/modulo/src/Controller/BibliotecaController.php
@@ -15,15 +15,9 @@ class BibliotecaController extends ControllerBase {
     $config = $this->config('modulo.biblioteca');
     $libros = $config->get('libros');
 
-    // Crear la salida para mostrar los libros
-    $output = '<h2>Lista de Libros</h2><ul>';
-    foreach ($libros as $libro) {
-      $output .= '<li>' . $libro['titulo'] . ' - ' . $libro['autor'] . ' (' . $libro['año'] . ')</li>';
-    }
-    $output .= '</ul>';
-
     return [
-      '#markup' => $output,
+      '#theme' => 'biblioteca',
+      '#libros' => $libros,
     ];
   }
 }
