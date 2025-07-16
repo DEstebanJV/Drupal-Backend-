68b8f3b86edcded52de91247040c5d99add95cb2
destebanjv <davidjv312@gmail.com>
2025-07-16 11:46:56 -0500

    Feat: update

--- web/modules/custom/job_queue/src/Controller/JobQueueController.php
+++ web/modules/custom/job_queue/src/Controller/JobQueueController.php
@@ -5,8 +5,6 @@ namespace Drupal\job_queue\Controller;
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\Core\Queue\QueueFactory;
 use Symfony\Component\DependencyInjection\ContainerInterface;
-use Symfony\Component\HttpFoundation\Response;
-use Drupal\Core\Session\AccountInterface;
 
 class JobQueueController extends ControllerBase {
 
@@ -18,18 +16,10 @@ class JobQueueController extends ControllerBase {
   protected QueueFactory $queueFactory;
 
   /**
-   * El usuario actual.
-   *
-   * @var \Drupal\Core\Session\AccountInterface
-   */
-  protected AccountInterface $currentUser;
-
-  /**
    * Constructor del controlador.
    */
-  public function __construct(QueueFactory $queue_factory, AccountInterface $current_user) {
+  public function __construct(QueueFactory $queue_factory) {
     $this->queueFactory = $queue_factory;
-    $this->currentUser = $current_user;
   }
 
   /**
@@ -37,8 +27,7 @@ class JobQueueController extends ControllerBase {
    */
   public static function create(ContainerInterface $container): self {
     return new static(
-      $container->get('queue'),
-      $container->get('current_user')
+      $container->get('queue')
     );
   }
 
@@ -46,19 +35,19 @@ class JobQueueController extends ControllerBase {
    * Agrega un trabajo a la cola.
    */
   public function enqueueJob(): array {
-    $uid = $this->currentUser->id();
-    $username = $this->currentUser->getDisplayName();
+    $uid = $this->currentUser()->id();
+    $username = $this->currentUser()->getDisplayName();
 
     $queue = $this->queueFactory->get('job_queue_example');
     $queue->createItem([
       'uid' => $uid,
       'username' => $username,
       'time' => time(),
-      'message' => 'Trabajo generado por el usuario actual',
+      'message' => 'Trabajo generado por el usuario actual segunda prueba',
     ]);
 
     return [
-      '#markup' => $this->t('Trabajo encolado para el usuario @user.', ['@user' => $username]),
+      '#markup' => $this->t('Trabajo para el usuario @user.', ['@user' => $username]),
     ];
   }
 }
