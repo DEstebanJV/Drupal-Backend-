1347fecd5d0f57282f689eaaf7ddde70cf866af3
David Esteban Jimenez <78695951+DEstebanJV@users.noreply.github.com>
2025-07-16 13:13:04 -0500

    Update JobQueueController.php

--- web/modules/custom/job_queue/src/Controller/JobQueueController.php
+++ web/modules/custom/job_queue/src/Controller/JobQueueController.php
@@ -5,49 +5,80 @@ namespace Drupal\job_queue\Controller;
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\Core\Queue\QueueFactory;
 use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Session\AccountInterface;
 
+/**
+ * Controlador para pruebas con colas en Drupal.
+ */
 class JobQueueController extends ControllerBase {
 
   /**
-   * La fábrica de colas.
+   * Fábrica de colas de Drupal.
    *
    * @var \Drupal\Core\Queue\QueueFactory
    */
   protected QueueFactory $queueFactory;
 
   /**
+   * Usuario actual.
+   *
+   * @var \Drupal\Core\Session\AccountInterface
+   */
+  protected AccountInterface $currentUser;
+
+  /**
    * Constructor del controlador.
    */
-  public function __construct(QueueFactory $queue_factory) {
+  public function __construct(QueueFactory $queue_factory, AccountInterface $current_user) {
     $this->queueFactory = $queue_factory;
+    $this->currentUser = $current_user;
   }
 
   /**
-   * {@inheritdoc}
+   * Método estático para la creación con inyección de dependencias.
    */
   public static function create(ContainerInterface $container): self {
     return new static(
-      $container->get('queue')
+      $container->get('queue'),
+      $container->get('current_user')
     );
   }
 
   /**
-   * Agrega un trabajo a la cola.
+   * Agrega 50 trabajos a la cola.
    */
-  public function enqueueJob(): array {
-    $uid = $this->currentUser()->id();
-    $username = $this->currentUser()->getDisplayName();
-
+  public function enqueueMultiple(): array {
+    $uid = $this->currentUser->id();
+    $username = $this->currentUser->getDisplayName();
     $queue = $this->queueFactory->get('job_queue_example');
-    $queue->createItem([
-      'uid' => $uid,
-      'username' => $username,
-      'time' => time(),
-      'message' => 'Trabajo generado por el usuario actual cuarta prueba',
-    ]);
+
+    for ($i = 1; $i <= 50; $i++) {
+      $queue->createItem([
+        'uid' => $uid,
+        'username' => $username,
+        'time' => time(),
+        'index' => $i,
+        'message' => "Trabajo #{$i} generado automáticamente",
+      ]);
+    }
 
     return [
-      '#markup' => $this->t('Trabajo para el usuario @user.', ['@user' => $username]),
+      '#markup' => $this->t('Se agregaron 50 trabajos a la cola para el usuario @user.', [
+        '@user' => $username,
+      ]),
     ];
   }
+
+  /**
+   * Muestra la cantidad de trabajos pendientes en la cola.
+   */
+  public function showQueueCount(): array {
+    $queue = $this->queueFactory->get('job_queue_example');
+    $count = $queue->numberOfItems();
+
+    return [
+      '#markup' => $this->t('Trabajos pendientes en la cola: @count', ['@count' => $count]),
+    ];
+  }
+
 }
