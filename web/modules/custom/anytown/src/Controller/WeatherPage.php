90fac2b2f17090114ab8a25c335e3aeb16708242
destebanjv <davidjv312@gmail.com>
2025-07-30 17:30:35 -0500

    Feat: refactor code in a services

--- web/modules/custom/anytown/src/Controller/WeatherPage.php
+++ web/modules/custom/anytown/src/Controller/WeatherPage.php
@@ -1,54 +1,41 @@
-<?php
+<?php 
 
 declare(strict_types=1);
 
 namespace Drupal\anytown\Controller;
 
-use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\anytown\ForecastClientInterface;
 use Drupal\Core\Controller\ControllerBase;
-#use Drupal\Core\DependencyInjection\AutowireTrait;
-use Drupal\Core\Logger\RfcLogLevel;
-use GuzzleHttp\ClientInterface;
-use GuzzleHttp\Exception\RequestException;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Controller for anytown.weather_page route.
  */
 class WeatherPage extends ControllerBase {
 
-  /* Uso de dependency injection sin el metodo de create */
-  #use AutowireTrait;
-  
-
   /**
-   * HTTP client.
+   * Forecast API client.
    *
-   * @var \GuzzleHttp\ClientInterface
+   * @var \Drupal\anytown\ForecastClientInterface
    */
-  private $httpClient;
-
-  /**
-   * Logging service, set to 'anytown' channel.
-   *
-   * @var \Psr\Log\LoggerInterface
-   */
-  private $logger;
+  private $forecastClient;
 
   /**
    * WeatherPage controller constructor.
    *
-   * @param \GuzzleHttp\ClientInterface $http_client
-   *   HTTP client.
+   * @param \Drupal\anytown\ForecastClientInterface $forecast_client
+   *   Forecast API client service.
    */
-  public function __construct(ClientInterface $http_client) {
-    $this->httpClient = $http_client;
-    $this->logger = $this->getLogger('anytown');
+  public function __construct(ForecastClientInterface $forecast_client) {
+    $this->forecastClient = $forecast_client;
   }
 
-  /* Uso de dependency injection con el metodo de create */
-  public static function create(ContainerInterface $container){
+  /**
+   * {@inheritDoc}
+   */
+  public static function create(ContainerInterface $container) {
     return new static(
-      $container->get('http_client')
+      $container->get('anytown.forecast_client')
     );
   }
 
@@ -59,25 +46,17 @@ class WeatherPage extends ControllerBase {
     // Style should be one of 'short', or 'extended'. And default to 'short'.
     $style = (in_array($style, ['short', 'extended'])) ? $style : 'short';
 
-    $url = 'https://raw.githubusercontent.com/DrupalizeMe/module-developer-guide-demo-site/main/backups/weather_forecast.json';
-    $data = NULL;
-
-    try {
-      $response = $this->httpClient->get($url);
-      $data = json_decode($response->getBody()->getContents());
-    }
-    catch (RequestException $e) {
-      $this->logger->log(RfcLogLevel::WARNING, $e->getMessage());
-    }
-
-    if ($data) {
+    $url = 'https://module-developer-guide-demo-site.ddev.site/modules/custom/anytown/data/weather_forecast.json';
+    $forecast_data = $this->forecastClient->getForecastData($url);
+    if ($forecast_data) {
       $forecast = '<ul>';
-      foreach ($data->list as $day) {
-        $weekday = ucfirst($day->day);
-        $description = array_shift($day->weather)->description;
-        // Convert units in Kelvin to Fahrenheit.
-        $high = round(($day->main->temp_max - 273.15) * 9 / 5 + 32);
-        $low = round(($day->main->temp_min - 273.15) * 9 / 5 + 32);
+      foreach ($forecast_data as $item) {
+        [
+          'weekday' => $weekday,
+          'description' => $description,
+          'high' => $high,
+          'low' => $low,
+        ] = $item;
         $forecast .= "<li>$weekday will be <em>$description</em> with a high of $high and a low of $low.</li>";
       }
       $forecast .= '</ul>';
@@ -88,6 +67,7 @@ class WeatherPage extends ControllerBase {
 
     $output = "<p>Check out this weekend's weather forecast and come prepared. The market is mostly outside, and takes place rain or shine.</p>";
     $output .= $forecast;
+    $output .= '<h3>Weather related closures</h3></h3><ul><li>Ice rink closed until winter - please stay off while we prepare it.</li><li>Parking behind Apple Lane is still closed from all the rain last week.</li></ul>';
 
     return [
       '#markup' => $output,
